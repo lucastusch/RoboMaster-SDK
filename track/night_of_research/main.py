a2112bc02f2ffc29c6e3274a383704d442ef1ade
Lucas Tusch <lucas.tusch@gmail.com>
2024-05-25 16:41:43 +0200

    Long night of research code update

--- track/night_of_research/main.py
+++ track/night_of_research/main.py
@@ -1,49 +1,101 @@
 import time
-import robomaster
+import random
+import pandas as pd
+from matplotlib.animation import FuncAnimation
+
+from robomaster import robot
+import matplotlib.pyplot as plt
+from itertools import count
+
+drive_counter: int = 0
+
+
+def animate(i: int):
+    x_vals.append(next(index))
+
+    voltage: float = get_ad_data(1, 1) * (12.6 / 1024)
+    current: float = get_ad_data(1, 2) * (12.6 / 1024)
+
+    power: float = (voltage * current) * 100
+
+    power_list.append((power * 12.6) / 1000)
+
+    plt.legend(["Power graph"])
+    plt.xlabel("time")
+    plt.ylabel("miliwatts")
+
+    plt.cla()
+    plt.plot(x_vals, power_list)
+
+    plt.legend(["power"])
+    plt.xlabel("time")
+    plt.ylabel("Watts")
+
+    plt.tight_layout()
+    plt.show()
+
+    drive_handler_backwards()
+
+
+def plot_recorded_data():
+    plt.style.use('fivethirtyeight')
+
+    plt.cla()
+    ani = FuncAnimation(plt.gcf(), animate)
+
+    plt.tight_layout()
+    plt.show()
 
 
 def get_ad_data(port_id: int, port: int):
     adc: int = ep_sensor_adaptor.get_adc(id=port_id, port=port)
-    print(f"Sensor adapter {port_id}-{port} adc is {adc}.")
+    # print(f"Sensor adapter {port_id}-{port} adc is {adc}.")
     return adc
 
 
-def drive_handler(right: int, mid: int, left: int):
-    if right < 303 & mid > 303 & left < 303:  # robot drives on track
-        ep_chassis.move(x=0.05, y=0, z=0, xy_speed=0.4).wait_for_completed()
+def drive_handler_forward():
+    ep_chassis.move(x=0.15, y=0, z=0, xy_speed=0.15).wait_for_completed()
 
-    elif right > 303 & mid < 303 & left < 303:  # robot drives to the right of the track
-        ep_chassis.move(x=0.05, y=0.007, z=0, xy_speed=0.4).wait_for_completed()
 
-    elif right < 303 & mid < 303 & left > 303:  # robot drives to the left of the track
-        ep_chassis.move(x=0.05, y=-0.007, z=0, xy_speed=0.4).wait_for_completed()
+def drive_handler_left():
+    ep_chassis.move(x=0, y=0.15, z=0, xy_speed=0.15).wait_for_completed()
 
-    else:  # robot's position unknown
-        print("No track found.")
+
+def drive_handler_backwards():
+    ep_chassis.move(x=-0.15, y=0, z=0, xy_speed=0.15).wait_for_completed()
+
+
+def drive_handler_right():
+    ep_chassis.move(x=0, y=-0.15, z=0, xy_speed=0.15).wait_for_completed()
 
 
 def main():
-    track_data_file_headers: list = ["right sensor", "mid sensor", "left sensor"]
+    i: int = 0
 
     while True:
-        right_sensor: int = get_ad_data(1, 1)
-        mid_sensor: int = get_ad_data(1, 2)
-        left_sensor: int = get_ad_data(2, 1)
+        if i == 10:
+            return False
 
-        drive_handler(right_sensor, mid_sensor, left_sensor)
+        # drive_handler()
+        plot_recorded_data()
 
-        voltage: int = get_ad_data(3, 1)
-        current: int = get_ad_data(3, 2)
-        temperature_left: int = get_ad_data(4, 1)
-        temperature_right: int = get_ad_data(4, 2)
+        i += 1
 
 
 if __name__ == '__main__':
-    ep_robot = robomaster.robot.Robot()
+    ep_robot = robot.Robot()
     ep_robot.initialize(conn_type="ap")
 
     ep_chassis = ep_robot.chassis
     ep_sensor_adaptor = ep_robot.sensor_adaptor
+
+    index = count()
+
+    x_vals: list = []
+
+    power_list: list = []
+    temperature_left_list: list = []
+    temperature_right_list: list = []
 
     main()
 
